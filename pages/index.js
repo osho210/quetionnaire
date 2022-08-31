9bf047efaf623462322d7deebca9d9d05c33f982
osho210 <anysense.yamatsu@gmail.com>
2022-09-01 02:40:06 +0900

    firebaseファイルの階層の移動(component)

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Input from "@material-ui/core/Input";
 import classes from "./style.module.css";
-import firebase from "./firebase";
+import firebase from "./component/firebase";
 
 export default function Home() {
   const { register, handleSubmit, formState: { errors } ,control} = useForm()
